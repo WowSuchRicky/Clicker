6f97e842badf5240b58dd119d675e5695af3d903
Nick Heindl <nheindl@wisc.edu>
2014-12-06 18:23:00 -0600

    added unique sounds for each building

--- public/scripts/update.js
+++ public/scripts/update.js
@@ -34,32 +34,53 @@ var update = {
     city: function(citynum){
         switch(citynum){
             case 1:
+                    document.getElementById("cityOneAudio").play();
+
                 break;
             case 2:
+                    document.getElementById("cityTwoAudio").play();
+
                 break;
             case 3:
+                    document.getElementById("cityThreeAudio").play();
+
                 break;
             case 4:
+                    document.getElementById("cityFourAudio").play();
+
                 break;
             case 5:
+                    document.getElementById("cityFiveAudio").play();
+
                 break;
             case 6:
+                    document.getElementById("citySixAudio").play();
+
                 break;
             case 7:
+                    document.getElementById("citySevenAudio").play();
+
                 break;
             case 8:
+                    document.getElementById("cityEightAudio").play();
+
                 break;
             case 9:
+                    document.getElementById("cityNineAudio").play();
+
                 break;
             case 10:
+                    document.getElementById("cityTenAudio").play();
+
                 break;
             case 11:
+                    document.getElementById("cityElevenAudio").play();
+
                 break;
             default:
                 break;
         }
 
-        document.getElementById("cityTwoAudio").play();
     }
 
 
